f226eb981cf9761f1121f289a0159330602b161d
Valyn Tyler <git@valyntyler.com>
2024-11-23 15:21:31 +0200

    update mod note to represent pitch agnostically

--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,10 +1,11 @@
 #[derive(Debug)]
-pub enum Note {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F, 
-    G,
+pub struct Note {
+    pub pitch: i8,
+}
+
+impl Note {
+    pub fn pitch(&self) -> i8 {
+        // to conform with MIDI, notes are constrained between 0 and 127
+        self.pitch.abs()
+    }
 }
