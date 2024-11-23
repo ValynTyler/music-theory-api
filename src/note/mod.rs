275193ecdf1f5546f78389e763ba18c38cfe8e14
Valyn Tyler <git@valyntyler.com>
2024-11-23 16:07:01 +0200

    feat: test discarding MSB for midi parity

--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Note {
     pub pitch: u8,
 }
@@ -14,5 +14,11 @@ impl Note {
 
     pub fn max() -> Self {
         Note { pitch: 127 }
+    }
+}
+
+impl From::<u8> for Note {
+    fn from(value: u8) -> Self {
+        Note { pitch: value % 128 }
     }
 }
--- tests/note_test.rs
+++ tests/note_test.rs
@@ -4,4 +4,9 @@ use music_theory_api::note::Note;
 fn test_midi_parity() {
     assert_eq!(0x00 as u8, Note::min().pitch());
     assert_eq!(0x7F as u8, Note::max().pitch());
+
+    for i in 0..u8::MAX / 2 + 1 {
+        assert_eq!(Note::from(i), Note::from(i));
+        assert_eq!(Note::from(i).pitch(), Note::from(i + 128).pitch());
+    }
 }
