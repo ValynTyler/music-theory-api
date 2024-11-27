05caf77cc2a7dce292922c200522ac4e1792edd8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-28 00:55:34 +0200

    feat: changed how im displaying AbsoluteNote

--- src/note/absolute/mod.rs
+++ src/note/absolute/mod.rs
@@ -1,10 +1,17 @@
+use std::fmt::Display;
+
 use crate::step::UStep;
 
-#[derive(Debug)]
 pub struct AbsoluteNote(UStep);
 
 impl From::<UStep> for AbsoluteNote {
     fn from(value: UStep) -> Self {
         Self(value)
+    }
+}
+
+impl Display for AbsoluteNote {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", u8::from(self.0))
     }
 }
