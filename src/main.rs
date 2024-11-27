64bc11ae9da9e6ce2fb74b164514eeab8b837b46
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-27 19:27:04 +0200

    implemented some structs to try and model notes

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,27 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+enum NoteLetter {
+    A, B, C, D, E, F, G
+}
+
+impl Display for NoteLetter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+enum NoteSymbol {
+    Sharp(u8), Flat(u8)
+}
+
+#[derive(Debug)]
+struct Note {
+    letter: NoteLetter,
+    symbol: NoteSymbol,
+}
+
 fn main() {
+    println!("{:?}", Note { letter: NoteLetter::A, symbol: NoteSymbol::Sharp(1) });
 }
