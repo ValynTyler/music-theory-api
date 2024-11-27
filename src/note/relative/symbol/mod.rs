ee7e4f42dfd36ac63c2f53a891ca392e9ae94646
Valyn Tyler <git@valyntyler.com>
2024-11-27 22:56:15 +0200

    feat: using conversions to get shift from NoteSymbol

--- src/note/relative/symbol/mod.rs
+++ src/note/relative/symbol/mod.rs
@@ -1,7 +1,17 @@
-use crate::step::{IStepNonZero, UStepNonZero};
+use crate::step::{IStep, UStep, UStepNonZero};
 
 pub enum NoteSymbol {
     Sharp(UStepNonZero),
     Flat(UStepNonZero),
     Natural,
+}
+
+impl From<NoteSymbol> for IStep {
+    fn from(value: NoteSymbol) -> Self {
+        match value {
+            NoteSymbol::Sharp(ustep_non_zero) => IStep::from(UStep::from(ustep_non_zero)),
+            NoteSymbol::Flat(ustep_non_zero) => IStep::from(UStep::from(ustep_non_zero)),
+            NoteSymbol::Natural => IStep::from(0),
+        }
+    }
 }
