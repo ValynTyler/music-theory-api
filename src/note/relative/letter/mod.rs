e2c79764ac24c516e1b163e1ff6967b6b7ce9462
Valyn Tyler <git@valyntyler.com>
2024-11-27 23:02:41 +0200

    feat: impl from `UStep` for `NoteLetter`

--- src/note/relative/letter/mod.rs
+++ src/note/relative/letter/mod.rs
@@ -1,3 +1,5 @@
+use crate::step::UStep;
+
 pub enum NoteLetter {
     C,
     D,
@@ -7,3 +9,17 @@ pub enum NoteLetter {
     A,
     B,
 }
+
+impl From::<NoteLetter> for UStep {
+    fn from(value: NoteLetter) -> Self {
+        match value {
+            NoteLetter::C => UStep::from(0),
+            NoteLetter::D => UStep::from(2),
+            NoteLetter::E => UStep::from(4),
+            NoteLetter::F => UStep::from(5),
+            NoteLetter::G => UStep::from(7),
+            NoteLetter::A => UStep::from(9),
+            NoteLetter::B => UStep::from(11),
+        }
+    }
+} 
