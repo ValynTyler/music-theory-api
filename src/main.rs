5aef83d659c47dab2a37cc49a58bd51afdc9a6a8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-27 20:23:24 +0200

    i think i actually might be on to something

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
-pub struct AbsoluteNote {
-    pitch: u8, // between 8 and 127
+struct NotePitch(u8); // between 0 and 11
+
+impl From::<u8> for NotePitch {
+    fn from(value: u8) -> Self {
+        Self(value % 12)
+    }
 }
 
-pub struct RelativeNote {
-    base: u8,           // between 0 and 11
-    symbol: i8,         // any value
-    pitch_class: u8,    // between 0 and 9
-                        // converted absolute value will be between 0 and 9 * 12 + 11 (119)
+impl From<NoteLetter> for NotePitch {
+    fn from(value: NoteLetter) -> Self {
+        match value {
+            NoteLetter::C => 0.into(),
+            NoteLetter::D => 2.into(),
+            NoteLetter::E => 4.into(),
+            NoteLetter::F => 5.into(),
+            NoteLetter::G => 7.into(),
+            NoteLetter::A => 9.into(),
+            NoteLetter::B => 11.into(),
+        }
+    }
+}
+
+enum NoteLetter {
+    A, B, C, D, E, F, G
+}
+
+enum NoteSymbol {
+    Flat(NotePitch),
+    Sharp(NotePitch),
+    Natural,
 }
 
 fn main() {
+    println!("{}", -13 % 122);
 }
