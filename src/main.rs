fafe291cb8feda7511eb14568d48bbd3f403721c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-27 19:11:51 +0200

    deprecated notation enums

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,2 @@
-use music_theory_api::notation::{note::{Letter, Solfege}, symbol::Symbol};
-
 fn main() {
-    println!("{:?}{}", Letter::from(Solfege::Si), Symbol::Flat);
 }
--- src/notation/note/mod.rs
+++ src/notation/note/mod.rs
@@ -1,33 +1,4 @@
-mod solfege;
-mod letter;
-
-pub use solfege::*;
-pub use letter::*;
-
-impl From::<Solfege> for Letter {
-    fn from(value: Solfege) -> Self {
-        match value {
-            Solfege::Do  => Letter::C,
-            Solfege::Re  => Letter::D,
-            Solfege::Mi  => Letter::E,
-            Solfege::Fa  => Letter::F,
-            Solfege::Sol => Letter::G,
-            Solfege::La  => Letter::A,
-            Solfege::Si  => Letter::B,
-        }
-    }
-}
-
-impl From<Letter> for Solfege {
-    fn from(value: Letter) -> Self {
-        match value {
-            Letter::A => Solfege::La,
-            Letter::B => Solfege::Si,
-            Letter::C => Solfege::Do,
-            Letter::D => Solfege::Re,
-            Letter::E => Solfege::Mi,
-            Letter::F => Solfege::Fa,
-            Letter::G => Solfege::Sol,
-        }
-    }
+pub enum Notation {
+    Solfege,
+    Letter
 }
